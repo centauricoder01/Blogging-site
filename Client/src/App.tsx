b7aed7b022792adb3be79af4251b205236d05f44
Rajendra <itsrajendrapatel@gmail.com>
2023-07-04 22:18:29 +0530

    Home page design started with router setup done

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signup from './Pages/Signup'
+import Loign from './Pages/Loign'
+import Dashboard from './Pages/Dashboard'
 
 const App = () => {
     return (
-        <div>
-            <h1>Let Start the Project</h1>
-        </div>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/signup' element={<Signup />}/>
+            <Route path='/login' element={<Loign />}/>
+            <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
     )
 }
 
---
+++ Client/src/Components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../Assets/logo.png';
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default Navbar
