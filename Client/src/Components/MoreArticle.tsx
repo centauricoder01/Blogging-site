7e14391f029ab14233bfded9941b826a7e79a726
Rajendra <itsrajendrapatel@gmail.com>
2023-07-07 19:49:23 +0530

    reference to more article file has been created

--- Client/src/Components/MoreArticle.tsx
+++ Client/src/Components/MoreArticle.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const MoreArticle = () => {
-  return (
-    <div>MoreArticle</div>
-  )
+    return (
+        <div>
+            <h1>Here we will add some reference of your article </h1>
+        </div>
+    )
 }
 
 export default MoreArticle
