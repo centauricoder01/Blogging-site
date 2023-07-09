a407557c5c0b6c3e25b071837cff465075a70a17
Rajendra <itsrajendrapatel@gmail.com>
2023-07-09 22:29:33 +0530

    Login Done

--- Client/src/Pages/Loign.tsx
+++ Client/src/Pages/Loign.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 const Loign = () => {
   return (
-    <div>Loign</div>
+    <>
+    <Navbar/>
+      <section className=" body-font relative ">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 ">Login to Your Account</h1>
+          </div>
+          <div className="lg:w-1/2 md:w-4/5 mx-auto sm:w-full">
+            <div className="flex flex-col	justify-center items-center -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">Email</label>
+                  <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">Password</label>
+                  <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
