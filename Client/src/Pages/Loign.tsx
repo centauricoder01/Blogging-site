7376a51b39285f790ec68de35d3fafc6ca63f0f8
Rajendra <itsrajendrapatel@gmail.com>
2023-07-10 12:30:50 +0530

    Signup Page design and input value fetching done

--- Client/src/Pages/Loign.tsx
+++ Client/src/Pages/Loign.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Components/Navbar'
 
 const Loign = () => {
+
+  interface FormValues {
+    email: string;
+    password: string;
+  }
+
+  const [formValues, setFormValues] = useState<FormValues>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Do something with the form values, such as sending them to an API or processing them.
+    console.log(formValues);
+  };
+
+
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <section className=" body-font relative ">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
@@ -15,19 +41,18 @@ const Loign = () => {
               <div className="p-2 w-1/2">
                 <div className="relative">
                   <label className="leading-7 text-sm ">Email</label>
-                  <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                  <input type="text" id="email" name="email" value={formValues.email} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 text-gray-900 focus:ring-indigo-200 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div className="p-2 w-1/2">
                 <div className="relative">
                   <label className="leading-7 text-sm ">Password</label>
-                  <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                  <input type="password" id="password" name="password" value={formValues.password} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div className="p-2 w-full">
-                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
+                <button onClick={handleSubmit} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
               </div>
-
             </div>
           </div>
         </div>
--- Client/src/Pages/Signup.tsx
+++ Client/src/Pages/Signup.tsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../Components/Navbar';
 
 const Signup = () => {
+
+  interface FormValues {
+    name: string;
+    username: string;
+    role: string;
+    email: string;
+    password: string;
+  }
+
+  const [formValues, setFormValues] = useState<FormValues>({
+    email: '',
+    password: '',
+    name: '',
+    username: '',
+    role: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Do something with the form values, such as sending them to an API or processing them.
+    console.log(formValues);
+  };
+
   return (
-    <div>Signup</div>
+    <div>
+      <Navbar />
+      <section className=" body-font relative ">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 ">Create an Account </h1>
+          </div>
+          <div className="lg:w-1/2 md:w-4/5 mx-auto sm:w-full">
+            <div className="flex flex-col	justify-center items-center -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">Name</label>
+                  <input type="text" id="name" name="name" value={formValues.name} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 text-gray-900 focus:ring-indigo-200 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">Email</label>
+                  <input type="email" id="email" name="email" value={formValues.email} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">UserName</label>
+                  <input type="text" id="username" name="username" value={formValues.username} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">Role</label>
+                  <input type="text" id="role" name="role" value={formValues.role} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm ">Password</label>
+                  <input type="password" id="password" name="password" value={formValues.password} onChange={handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button onClick={handleSubmit} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
